2a242b19531e9eeba09aff6660c19b8f939a12e2
fairoos <fairoosnm123@gmail.com>
2019-03-28 01:11:23 +0530

    GREEN: Adds remove_stop_words

--- classifier.py
+++ classifier.py
@@ -20,3 +20,16 @@ def clean_data(data):
     contents_list.sort()
     contents_list = [x.lower() for x in contents_list] #to lower case all words
     return contents_list
+
+def remove_stop_words(input_data):
+    cwd = current_working_dir()
+    stop_words_file = '{}/stop_words_dir/stop_words.txt'.format(cwd)
+    stop_words_file = open(stop_words_file)
+    contents = stop_words_file.read()
+    list_contents = contents.split()
+    all_words = input_data
+    words = list()
+    for word in all_words: 
+        if word not in list_contents:
+            words.append(word)    
+    return words
