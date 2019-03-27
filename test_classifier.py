2108ecc621099258008c05514948e6dc99537fc1
fairoos <fairoosnm123@gmail.com>
2019-03-28 00:20:55 +0530

    RED:Adds test for Path_test_data.

--- test_classifier.py
+++ test_classifier.py
@@ -10,3 +10,6 @@ def test_current_working_dir():
 
 def test_path_train_data():
     assert classifier.path_train_data() == "{}/training_data/".format(cwd)
+
+def test_test_data():
+    assert classifier.path_test_data() == "{}/testing_data".format(cwd)
