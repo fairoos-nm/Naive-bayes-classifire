1e1171a47bc73e0848f7ed31b4a6c57c8b4cd4b6
fairoos <fairoosnm123@gmail.com>
2019-03-28 09:57:24 +0530

    RED:Adds test for words_in_dir.

--- test_classifier.py
+++ test_classifier.py
@@ -20,6 +20,9 @@ def test_clean_data():
 
 def test_remove_stop_words():
     classifier.remove_stop_words(['am', 'hello', 'here', 'i']) == ['hello']
-
+    
+def test_words_in_dir():
+    assert classifier.words_in_dir(demo_train_data) == ['am', 'hello', 'here', 'i']
+    
 def test_train_data():
     assert classifirer.train_data(demo_train_data) == {"sports_data":{"grate":1, "game":2, "very":1, "clean":2, "match":1, "forgettable":1}, "non_sports_data":{"election":2, "close":1}}
